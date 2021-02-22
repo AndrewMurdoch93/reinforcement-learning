30d7f78920e335c52427199799643b76de9adb58
Andrew Murdoch <andrew.murdoch93@gmail.com>
2021-02-22 16:22:33 +0200

    random walk first attempt

---
+++ Examples/Random walk/agent.py
@@ -0,0 +1,10 @@
+import random
+
+def random_policy():
+    random.seed()
+    direction = random.uniform(0,1)
+    
+    if direction>0.5:
+        return 'r'
+    elif direction<=0.5:
+        return 'l'
--- Examples/Random walk/main.py
+++ Examples/Random walk/main.py
@@ -1,4 +1,32 @@
-import sys
+import numpy as np
+from agent import random_policy
 
-print(sys.version)
-print(sys.executable)
+state=1
+returns=np.array([0, 0, 0, 0, 0])
+history=[]
+G=0
+t=0
+gamma=0.9
+
+while state<5:
+    t=t+1
+    direction = random_policy()
+
+    if direction == 'l':
+        newstate = state-1
+    elif direction == 'r':
+        newstate = state+1
+
+    if newstate<1:
+        newstate=1
+
+    if state==5:
+        reward=1
+    elif state!=5:
+        reward=0
+
+    history.append([state, direction, reward])
+    state=newstate
+    
+for x in range(t, 1):
+    print(history[x][1])
